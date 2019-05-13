6b24683cb98e1f9e4d7fb8a06d4197e021cfcb4d
meta_kotaro <kotaro@metaphase.co.jp>
2019-05-13 16:35:07 +0900

    webpackで起きていたエラーを修正

--- webpack.config.js
+++ webpack.config.js
@@ -5,48 +5,49 @@ const UglifyEsPlugin = require('uglify-es-webpack-plugin');
 
 const entries = {};
 glob.sync("./root/Templates/dev/**/entry.js", {
-	ignore: './root/Templates/dev/**/_*.js'
+    ignore: './root/Templates/dev/**/_*.js'
 }).map(function (file) {
-	const regEx = new RegExp(`./root/Templates/dev`);
-	const fileOriginalName = file.replace(regEx, '../../..');
-	const key = fileOriginalName.replace('entry.js', 'bundle.js');
-	const name = fileOriginalName.replace('entry.js', '');
-	entries[key] = file;
+    const regEx = new RegExp(`./root/Templates/dev`);
+    const fileOriginalName = file.replace(regEx, '../../..');
+    const key = fileOriginalName.replace('entry.js', 'bundle.js');
+    const name = fileOriginalName.replace('entry.js', '');
+    entries[key] = file;
 });
 
 module.exports = {
-	entry: entries,
-	output: {
-		filename: '[name]'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				use: [
-					{
-						loader: 'babel-loader',
-						options: {
-							presets: [
-								['env', {
-									'modules': false
-								}]
-							]
-						}
-					}
-				]
-			}
-		]
-	},
-	plugins: [
-		new webpack.ProvidePlugin({
-			$: 'jquery',
-			jQuery: 'jquery'
-		})
-	]
+    entry: entries,
+    mode: 'production',
+    output: {
+        filename: '[name]'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['env', {
+                                    'modules': false
+                                }]
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+        new UglifyEsPlugin()
+    ]
 
-	//devtool: 'source-map',
-	/*plugins: [
-        new UglifyEsPlugin(),
-	]*/
+    //devtool: 'source-map',
+    /*plugins: [
+    ]*/
 };
