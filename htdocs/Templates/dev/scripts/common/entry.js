1df65719d2b3fdee0a9fa55afa2170242a35f8b9
meta_kotaro <kotaro@metaphase.co.jp>
2020-07-15 18:54:22 +0900

    common.jsが不要のためentry.jsに統合

--- htdocs/Templates/dev/scripts/common/entry.js
+++ htdocs/Templates/dev/scripts/common/entry.js
@@ -1,11 +1,74 @@
 import "@babel/polyfill";
 
-// common
-import * as common from './common';
+// Layzr js
+import Layzr from 'layzr.js';
 
-let timer = false;
+// picturefill js
+import picturefill from 'picturefill';
+picturefill();
 
+// css browser selector js
+//import './libs/css_browser_selector';
 
+export const breakpoint = 768;
+
+let timer = false,
+  contentWidth;
+
+// ページロード中は画面見せないようにする
+const body = document.body;
+const loadWindow = '<div class="loadWindow">';
+body.insertAdjacentHTML('afterbegin', loadWindow);
+
+// デバイス判定
+export const _ua = ((u) => {
+  return {
+    Tablet:(u.indexOf('windows') != -1 && u.indexOf('touch') != -1 && u.indexOf('tablet pc') == -1)
+    || u.indexOf('ipad') != -1
+    || (u.indexOf('android') != -1 && u.indexOf('mobile') == -1)
+    || (u.indexOf('firefox') != -1 && u.indexOf('tablet') != -1)
+    || u.indexOf('kindle') != -1
+    || u.indexOf('silk') != -1
+    || u.indexOf('playbook') != -1,
+    Mobile:(u.indexOf('windows') != -1 && u.indexOf('phone') != -1)
+    || u.indexOf('iphone') != -1
+    || u.indexOf('ipod') != -1
+    || (u.indexOf('android') != -1 && u.indexOf('mobile') != -1)
+    || (u.indexOf('firefox') != -1 && u.indexOf('mobile') != -1)
+    || u.indexOf('blackberry') != -1
+  };
+})(window.navigator.userAgent.toLowerCase());
+
+// phone to のPC非活性化
+const phoneLink = () => {
+  const phoneTarget = document.querySelectorAll('a[href*="tel:"]');
+  const phoneNumArr = Array.prototype.slice.call(phoneTarget);
+  phoneNumArr.forEach((target) => {
+    target.addEventListener('click', (e) => {
+      if(!_ua.Mobile){
+        e.preventDefault();
+      }
+    });
+  });
+};
+
+// スムーススクロール
+const sroothscroll = () => {
+  const ancTarget = document.querySelectorAll('.pagetop > a[href*="#document"], a[href*="#anc_"]');
+  const ancTargetArr = Array.prototype.slice.call(ancTarget);
+  ancTargetArr.forEach((target) => {
+    target.addEventListener('click', (e) => {
+      let getId = target.getAttribute('href').replace('#', '');
+      let offset = document.getElementById(getId).offsetTop;
+
+      window.scrollTo({
+        top: offset,
+        behavior: "smooth"
+      });
+      e.preventDefault();
+    });
+  });
+};
 
 // グロナビ設定 ※サイト構造に応じて変更
 const gnav = () => {
@@ -29,7 +92,7 @@ const gnav = () => {
 
 const gnavSet = () => {
   if(document.querySelector('#gnav .menu') != null){
-    if(window.innerWidth < common.breakpoint){
+    if(window.innerWidth < breakpoint){
       document.querySelector('.menu + ul').style.display = 'none';
     }else{
       document.querySelector('#gnav .menu > *').classList.remove('is_active');
@@ -38,8 +101,33 @@ const gnavSet = () => {
   }
 };
 
+// 画像遅延読み込み
+const layzr = Layzr({
+  normal: 'data-normal',
+  threshold: 10
+});
+/*layzr.on('src:after', image => {
+  // 画像の書き換えをした後に実行
+  //image.classList.add('is-show'); // 今回はクラス名を追加してみました。
+});*/
+
+document.addEventListener('DOMContentLoaded', () => {
+  contentWidth = window.innerWidth;
+
+  // 画像遅延読み込み実行
+  layzr.update().check().handlers(true);
+});
+
 window.addEventListener('load', () => {
+  phoneLink();
+  sroothscroll();
   gnav();
+
+  const loadWindow = document.querySelector('.loadWindow');
+  loadWindow.classList.add('is_anim');
+  setTimeout(() => {
+    loadWindow.parentNode.removeChild(loadWindow);
+  }, 300);
 });
 
 window.addEventListener('resize', () => {
@@ -48,9 +136,9 @@ window.addEventListener('resize', () => {
   }
   timer = setTimeout(() => {
     let nowSize = window.innerWidth;
-    if(common.contentWidth != nowSize){
+    if(contentWidth != nowSize){
       gnavSet();
-      common.contentWidth = window.innerWidth;
+      contentWidth = window.innerWidth;
     }
   }, 50);
 });
